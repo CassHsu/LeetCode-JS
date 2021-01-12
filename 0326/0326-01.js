c7b400b521450f98b849fa08218c7aa05f57e2d0
Cass Hsu <cass.hsu@gmail.com>
2021-01-12 23:26:08 +0800

    init 326 & Approach 1

---
+++ 0326/0326-01.js
@@ -0,0 +1,10 @@
+var isPowerOfThree = function(n) {
+    let p = 1;
+    for (let i = 0; i < 31; i++) {
+        if (p == n) {
+            return true;
+        }
+        p *= 3;
+    }
+    return false;
+};
