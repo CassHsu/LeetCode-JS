a10c2d19ca1f18b5ef514b14b28d2d44ba955525
Cass Hsu <cass.hsu@gmail.com>
2021-01-31 11:18:21 +0800

    Approach 1: forloop

---
+++ 0035/0035-01.js
@@ -0,0 +1,12 @@
+var searchInsert = function(nums, target) {
+    const size = nums.length;
+    if (target > nums[size - 1]) return size;
+    let res = 0;
+    for (let i = 0; i < size; i++) {
+        if (nums[i] >= target) {
+            res = i;
+            break;
+        }
+    }
+    return res;
+};
