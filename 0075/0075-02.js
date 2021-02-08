4cec7dd135da7fe4b4e1673d1f8c948870249fb7
Cass Hsu <cass.hsu@gmail.com>
2021-02-08 23:40:25 +0800

    Approach 2: Count Array

---
+++ 0075/0075-02.js
@@ -0,0 +1,20 @@
+var sortColors = function(nums) {
+    const count = [0, 0, 0];
+    for (let n of nums) {
+        count[n]++;
+    }
+    
+    for (let i in nums) {
+        if (count[0] > 0) {
+            nums[i] = 0;
+            count[0]--;
+            
+        } else if (count[1] > 0) {
+            nums[i] = 1;
+            count[1]--;
+            
+        } else {
+            nums[i] = 2;
+        }
+    }
+};
