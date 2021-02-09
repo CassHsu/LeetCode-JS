8430a6aa5eed11d4925ec8048d1d3517de0ad3f7
Cass Hsu <cass.hsu@gmail.com>
2021-02-09 23:22:01 +0800

    Approach 1: Bubble Sort

---
+++ 0075/0075-01.js
@@ -0,0 +1,17 @@
+var sortColors = function(nums) {
+    let isConti = true;
+    let stop = 1;
+    while (isConti) {
+        isConti = false;
+        let count = nums.length - stop;
+        for (let i = 0; i < count; i++) {
+            if (nums[i] > nums[i+1]) {
+                let tmp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = tmp;
+                isConti = true;
+            }
+        }
+        stop++;
+    }
+};
