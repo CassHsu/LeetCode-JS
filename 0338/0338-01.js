be39aa30c539f02d13c29edcababb64d17e08c6d
Cass Hsu <cass.hsu@gmail.com>
2021-02-10 21:47:17 +0800

    Approach 1

---
+++ 0338/0338-01.js
@@ -0,0 +1,16 @@
+var countBits = function(num) {
+    const ret = [];
+    ret.push(0);
+    
+    for (let i = 1; i <= num; i++) {
+        let count = 0;
+        for (let offset = 30; offset >= 0; offset--) {
+            let mask = 1 << offset;
+            if ((i & mask)) {
+                count++;
+            }
+        }
+        ret.push(count);
+    }
+    return ret;
+};
