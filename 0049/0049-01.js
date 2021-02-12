7f5e95494c91a2a1dd29aa1789ef849a74083de0
Cass Hsu <cass.hsu@gmail.com>
2021-02-12 10:00:32 +0800

    Approach 1: Map

---
+++ 0049/0049-01.js
@@ -0,0 +1,22 @@
+var groupAnagrams = function(strs) {
+    if (strs.length === 0) return [];
+    if (strs.length === 1) return [strs];
+
+    const m = new Map();
+    
+    for (let s of strs) {
+        const k = [...s].sort().join('');
+        
+        if (m.has(k)) {
+            m.get(k).push(s);
+        } else {
+            m.set(k, [s]);
+        }
+    }
+    
+    const ret = [];
+    for (const [_, list] of m.entries()) {
+        ret.push(list);
+    }
+    return ret;
+};
