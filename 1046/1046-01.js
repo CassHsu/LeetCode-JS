c18819b3e6f5dfd34c4011c6966c31586ba27176
CassHsu <cass.hsu@gmail.com>
2021-03-02 11:50:36 +0800

    Approach 1: Sort + Recursion

---
+++ 1046/1046-01.js
@@ -0,0 +1,6 @@
+var lastStoneWeight = function(stones) {
+    if (stones.length === 1) return stones[0];
+    
+    const [m1, m2, ...others] = stones.sort((a, b) => b - a);
+    return lastStoneWeight([...others, m1 - m2]);
+};
