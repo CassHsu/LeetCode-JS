c646f64251fb1b7e06fdc88c8dfec87989344e0d
CassHsu <cass.hsu@gmail.com>
2021-03-08 11:35:56 +0800

    Create 0021-01.js

---
+++ 0021/0021-01.js
@@ -0,0 +1,19 @@
+var mergeTwoLists = function(l1, l2) {
+    let head = new ListNode();
+    let curr = head;
+    
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
+        } else {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+    
+    if (l1) curr.next = l1;
+    if (l2) curr.next = l2;
+    return head.next;
+};
