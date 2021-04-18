f8b018954dca55fb826c4225feab40998fe42230
CassHsu <cass.hsu@gmail.com>
2021-04-18 14:23:45 +0800

    Create 0459-01.js

---
+++ 0459/0459-01.js
@@ -0,0 +1,9 @@
+var repeatedSubstringPattern = function(s) {
+    const size = s.length;
+    if (size == 0) return false;
+    
+    let ss = s + s;
+    ss = ss.slice(1, size*2-1);
+    
+    return ss.includes(s);
+};
