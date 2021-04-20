251a5d59eecf3dc169c16b76b3f9c58bc736128e
CassHsu <cass.hsu@gmail.com>
2021-04-20 14:49:51 +0800

    Create 1827-02.js

---
+++ 1827/1827-02.js
@@ -0,0 +1,12 @@
+var minOperations = function(nums) {
+    let count = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i-1] >= nums[i]) {
+            let diff = nums[i-1] - nums[i];
+            diff++;
+            count += diff;
+            nums[i] += diff;
+        }
+    }
+    return count;
+};
