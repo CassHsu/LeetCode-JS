70e19b0831c5a74e61ead1595c6457881c90b340
CassHsu <cass.hsu@gmail.com>
2021-04-25 09:51:18 +0800

    Approach 2: Dynamic Programming

---
+++ 0070/0070-02.js
@@ -0,0 +1,15 @@
+var climbStairs = function(n) {
+    if (n == 1) return 1;
+    
+    const dp = [];
+    for (let i = 0; i <= n; i++) {
+        dp.push(0);
+    }
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+};
