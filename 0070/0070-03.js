edea60502c8c2c3b4f0207764f97146584a6d87c
CassHsu <cass.hsu@gmail.com>
2021-04-26 13:48:53 +0800

    Approach 3: Fibonacci Number

---
+++ 0070/0070-03.js
@@ -0,0 +1,12 @@
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    
+    let prev = 1;
+    let curr = 2;
+    for (let i = 3; i <= n; i++) {
+        const tmp = prev + curr;
+        prev = curr;
+        curr = tmp;
+    }
+    return curr;
+};
