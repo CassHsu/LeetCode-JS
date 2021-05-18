3e0a6d15e6faf579d745909d0743328dd85eab88
CassHsu <cass.hsu@gmail.com>
2021-05-18 18:24:34 +0800

    Approach 1: DP

---
+++ 0119/0119-01.js
@@ -0,0 +1,16 @@
+var getRow = function(rowIndex) {
+    const ret = [[1]];
+    
+    for (let i = 1; i <= rowIndex; i++) {
+        const row = [];
+        row.push(1);
+        
+        for (let j = 1; j < i; j++) {
+            row.push(ret[i-1][j-1] + ret[i-1][j]);
+        }
+        row.push(1);
+        
+        ret.push(row);
+    }
+    return ret[rowIndex];
+};
