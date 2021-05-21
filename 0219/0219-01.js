78e9547d429a5d90106934ee7aba14a4455c5244
CassHsu <cass.hsu@gmail.com>
2021-05-21 20:19:41 +0800

    Approach 1: Map

---
+++ 0219/0219-01.js
@@ -0,0 +1,10 @@
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.get(nums[i])) {
+            if (i <= map.get(nums[i])) return true;
+        }
+        map.set(nums[i], i + k);
+    }
+    return false;
+};
