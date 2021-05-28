f25db4bb16803fb79d790f140d69cc8ef25933d5
CassHsu <cass.hsu@gmail.com>
2021-05-28 20:15:19 +0800

    Create 0485-01.js

---
+++ 0485/0485-01.js
@@ -0,0 +1,15 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let max1 = 0;
+    let count = 0;
+    
+    for (let n of nums) {
+        if (n == 1) {
+            count++;
+        } else {
+            count = 0;
+        }
+        
+        if (count > max1) max1 = count;
+    }
+    return max1;
+};
