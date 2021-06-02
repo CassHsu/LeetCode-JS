f07cdc40113c41f845950680fe518831f40740b2
CassHsu <cass.hsu@gmail.com>
2021-06-02 20:42:14 +0800

    Update 0205-01.js

--- 0205/0205-01.js
+++ 0205/0205-01.js
@@ -1,10 +1,12 @@
-var isIsomorphic = function(s, t) {
-    const m1 = new Map();
-    const m2 = new Map();
-    for(let i = 0; i < s.length; i++){
-				if(m1[s[i]] !== m2[t[i]]) return false;
-		    m1[s[i]] = i;
-	      m2[t[i]] = i;         
+function isIsomorphic(s: string, t: string): boolean {
+    const ms = new Map();
+    const mt = new Map();
+    
+    for (let i = 0; i < s.length; i++) {
+        if (ms.has(s[i]) === false) ms.set(s[i], i);
+        if (mt.has(t[i]) === false) mt.set(t[i], i);
+        
+        if (ms.get(s[i]) !== mt.get(t[i])) return false;
     }
     return true;
 };
