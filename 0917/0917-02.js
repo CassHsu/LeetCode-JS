774b074b2b85a52a1b22b5e1fdf8e44f70c99907
CassHsu <cass.hsu@gmail.com>
2021-06-16 18:29:59 +0800

    Update 0917-02.js

--- 0917/0917-02.js
+++ 0917/0917-02.js
@@ -1,23 +1,24 @@
-function reverseOnlyLetters(s: string): string {
+var reverseOnlyLetters = function(s) {
     const ret = [];
-    const ss = s.split("");
     const letters = [];
+    const ss = s.split("");
     
-    const isalpha = (c: string) => {
+    const isalpha = (c) => {
         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
     }
     
     ss.forEach(c => {
-        if (isalpha(c)) letters.push(c)
+        if (isalpha(c)) letters.push(c);
     });
     
     ss.forEach(c => {
         if (isalpha(c)) {
             ret.push(letters.pop());
+            
         } else {
             ret.push(c);
         }
     });
     
-    return ret.join("");
+    return ret.join("")
 };
