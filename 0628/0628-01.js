b67d5e4da1a5daefe3e1de669dfa45040ef2b2ff
CassHsu <cass.hsu@gmail.com>
2021-06-22 21:47:32 +0800

    Create 0628-01.js

---
+++ 0628/0628-01.js
@@ -0,0 +1,20 @@
+var calPoints = function(ops) {
+    const stack = [];
+    
+    ops.forEach(op => {
+        if (op === "+") {
+            stack.push(stack[stack.length - 2] + stack[stack.length - 1]);
+            
+        } else if (op === "C") {
+            stack.pop();
+        } else if (op === "D") {
+            stack.push(2 * stack[stack.length - 1]);
+        } else {
+            stack.push(parseInt(op));
+        }
+    });
+    
+    let ans = 0;
+    stack.forEach(s => ans += s);
+    return ans;
+};
