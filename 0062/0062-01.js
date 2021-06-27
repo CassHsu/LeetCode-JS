bdece2ace8f21792eb5244d9fd573e9014aa2685
CassHsu <cass.hsu@gmail.com>
2021-06-27 14:44:12 +0800

    Approach 1: DP  + Loop

---
+++ 0062/0062-01.js
@@ -0,0 +1,21 @@
+var uniquePaths = function(m, n) {
+    const dp = [];
+    for (let i = 0; i < m; i++) {
+        const row = [];
+        for (let j = 0; j < n; j++) {
+            if (i == 0) row.push(1)
+            else row.push(0);
+            
+            if (j == 0) row[0] = 1;
+        }
+        dp.push(row);
+    }
+    
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    
+    return dp[m-1][n-1];
+};
