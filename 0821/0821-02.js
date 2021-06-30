e3151816599e8ee2943aff6e3c414a8655519168
CassHsu <cass.hsu@gmail.com>
2021-06-30 20:50:35 +0800

    Create 0821-02.js

---
+++ 0821/0821-02.js
@@ -0,0 +1,17 @@
+var shortestToChar = function(s, c) {
+    const size = s.length;
+    const ans = [];
+    const pos = [];
+    
+    for (let i = 0; i < size; i++) {
+        if (s[i] === c) pos.push(i); 
+    }
+    
+    for (let i = 0; i < size; i++) {
+        let n = size;
+        pos.forEach(p => { n = Math.min(n, Math.abs(i - p)) });
+        ans.push(n);
+    }
+    
+    return ans;
+};
