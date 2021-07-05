3b239b0764b0f898bec5dc06d30041ed35a5a661
CassHsu <cass.hsu@gmail.com>
2021-07-05 20:41:18 +0800

    Create 0374-01.js

---
+++ 0374/0374-01.js
@@ -0,0 +1,19 @@
+var guessNumber = function(n) {
+    let low = 1;
+    let high = n;
+    
+    while (low <= high) {
+        const mid = Math.floor(low + (high - low) / 2);
+        const res = guess(mid);
+        
+        if (res === 0) {
+            return mid;
+        } else if (res < 0) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return -1;
+};
