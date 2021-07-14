db5d5bb693b7b801f7dd98ac56007964d7f98aa8
CassHsu <cass.hsu@gmail.com>
2021-07-14 23:38:18 +0800

    Approach 2: Iterative

---
+++ 0145/0145-02.js
@@ -0,0 +1,15 @@
+var postorderTraversal = function(root) {
+    const ans = [];
+    const stack = [];
+    
+    if (root) stack.push(root);
+    
+    while (stack.length > 0) {
+        const node = stack.pop();
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+        ans.push(node.val);
+    }
+    
+    return ans.reverse();
+};
