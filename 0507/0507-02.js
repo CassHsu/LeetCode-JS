9ef6e0bc730440fe14c8296cc8b5f68d5a2644e8
CassHsu <cass.hsu@gmail.com>
2021-07-18 13:59:39 +0800

    Create 0507-02.js

---
+++ 0507/0507-02.js
@@ -0,0 +1,14 @@
+var checkPerfectNumber = function(num) {
+    if (num <= 0) return false;
+    
+    let sum = 0;
+    let half = num >> 1;
+    
+    for (let i = 1; i <= half; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    
+    return sum == num;
+};
