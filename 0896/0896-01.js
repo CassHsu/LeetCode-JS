6247bfd0153bbe7310956328e575745480f96efe
CassHsu <cass.hsu@gmail.com>
2021-07-26 23:30:11 +0800

    Create 0896-01.js

---
+++ 0896/0896-01.js
@@ -0,0 +1,17 @@
+var isMonotonic = function(nums) {
+    let mode = -1;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] < nums[i]) {
+            if (mode === 1) return false;
+            else mode = 0;
+        }
+        
+        if (nums[i - 1] > nums[i]) {
+            if (mode === 0) return false;
+            else mode = 1;
+        }
+    }
+    
+    return true;
+};
