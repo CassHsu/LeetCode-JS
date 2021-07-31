80d397925cc115a94ba005f725201822a1205431
CassHsu <cass.hsu@gmail.com>
2021-07-31 20:28:32 +0800

    Approach 2: Compare With Top-Left Neighbor

---
+++ 0766/0766-02.js
@@ -0,0 +1,10 @@
+var isToeplitzMatrix = function(matrix) {
+    for (const [r, row] of matrix.entries()) {
+        for (const [c, v] of row.entries()) {
+            if (r > 0 && c > 0 && v !== matrix[r - 1][c - 1])
+                return false;
+        }
+    }
+    
+    return true;
+};
