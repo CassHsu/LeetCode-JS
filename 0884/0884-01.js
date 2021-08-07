b41d5d8a9bf582180da034ebf96ab40ec0bf24ef
CassHsu <cass.hsu@gmail.com>
2021-08-07 17:48:45 +0800

    Create 0884-01.js

---
+++ 0884/0884-01.js
@@ -0,0 +1,24 @@
+var uncommonFromSentences = function(s1, s2) {
+    const m = new Map();
+    const ret = [];
+    
+    const as1 = s1.split(" ");
+    const as2 = s2.split(" ");
+    
+    as1.forEach(w => {
+        const v = m.get(w);
+        if (v) m.set(w, v + 1);
+        else m.set(w, 1);
+    });
+    
+    as2.forEach(w => {
+        const v = m.get(w);
+        if (v) m.set(w, v + 1);
+        else m.set(w, 1);
+    });
+    
+    m.forEach((v, k) => { 
+        if (v === 1) ret.push(k);
+    });
+    return ret;
+};
