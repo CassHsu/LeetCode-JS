f54d0437dfa2158dc60155184888b2333337509a
CassHsu <cass.hsu@gmail.com>
2021-08-09 16:36:06 +0800

    Create 1945-02.js

---
+++ 1945/1945-02.js
@@ -0,0 +1,12 @@
+var getLucky = function(s, k) {
+    let tmp = "";
+    for (const c of s) tmp += (c.charCodeAt() - 96).toString(10);
+    while (k--) {
+        tmp = ((ss) => {
+            let sum = 0;
+            for (const c of ss) sum += parseInt(c, 10);
+            return sum.toString(10);
+        })(tmp);
+    }
+    return parseInt(tmp, 10);
+};
