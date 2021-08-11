1541bd6a7fa2e14a88494c01e943e0490b719a77
CassHsu <cass.hsu@gmail.com>
2021-08-11 22:31:12 +0800

    Approach 1: subsets

---
+++ 1863/1863-01.js
@@ -0,0 +1,19 @@
+var subsetXORSum = function(nums) {
+    const subsets = [[]];
+    let sum = 0;
+    
+    for(const n of nums){
+        const last = subsets.length - 1;
+        for(let i = 0; i <= last; i++) {
+            subsets.push([...subsets[i], n]);
+        }
+    }
+    
+    for(let i = 0; i < subsets.length; i++){
+        if (subsets[i].length === 0) sum += 0
+        else if (subsets[i].length === 1) sum += subsets[i][0];
+        else sum += subsets[i].reduce((acc, curr) => acc ^ curr);
+    }
+    
+    return sum;
+};
