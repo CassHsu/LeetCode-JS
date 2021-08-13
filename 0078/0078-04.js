8c850617d3321404200f13972899d9c8b3dd6fb3
CassHsu <cass.hsu@gmail.com>
2021-08-13 11:03:02 +0800

    Approach 2: Backtracking

---
+++ 0078/0078-04.js
@@ -0,0 +1,21 @@
+var subsets = function(nums) {
+    const ret = [];
+    const curr = [];
+    
+    const backtrack = (i) => {
+        if (i >= nums.length) {
+            ret.push([...curr]);
+            return;
+        }
+        
+        curr.push(nums[i]);
+        backtrack(i + 1);
+        
+        curr.pop();
+        backtrack(i + 1);
+        return;
+    }
+    
+    backtrack(0);
+    return ret;
+};
