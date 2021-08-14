30e41ae9cf36954e722feb1063c349f43fa62b19
CassHsu <cass.hsu@gmail.com>
2021-08-14 12:43:55 +0800

    Approach 1: Backtracking

---
+++ 0046/0046-01.js
@@ -0,0 +1,20 @@
+var permute = function(nums) {
+    const ans = [];
+    const backtrack = (curr) => {
+        if (curr.length === nums.length) {
+            ans.push([...curr]);
+            return;
+        }
+        
+        for (const n of nums) {
+            if (curr.includes(n)) continue;
+            
+            curr.push(n);
+            backtrack(curr);
+            curr.pop();
+        }
+    }
+    
+    backtrack([]);
+    return ans;
+};
