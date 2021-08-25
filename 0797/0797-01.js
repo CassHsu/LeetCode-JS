59269d99f824af2266d8f0e011f90a65be172126
CassHsu <cass.hsu@gmail.com>
2021-08-25 11:05:39 +0800

    Backtracking

---
+++ 0797/0797-01.js
@@ -0,0 +1,18 @@
+var allPathsSourceTarget = function(graph) {
+    const ans = [];
+    const backtrack = (path, curr) => {
+        if (curr + 1 === graph.length) {
+            ans.push([...path]);
+            return;
+        }
+        
+        for (const n of graph[curr]) {
+            path.push(n);
+            backtrack(path, n);
+            path.pop();
+        }
+    }
+    
+    backtrack([0], 0);
+    return ans;
+};
