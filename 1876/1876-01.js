6fdba8be67677b3ea28e3effcf1700e219b5fe57
CassHsu <cass.hsu@gmail.com>
2021-08-29 11:35:56 +0800

    Create 1876-01.js

---
+++ 1876/1876-01.js
@@ -0,0 +1,11 @@
+var countGoodSubstrings = function(s) {
+    let count = 0;
+    
+    for (let i = 1; i < s.length - 1; i++) {
+        if (s[i-1] !== s[i] && s[i] !== s[i+1] && s[i-1] !== s[i+1]) {
+            count++;
+        }
+    }
+    
+    return count;
+};
