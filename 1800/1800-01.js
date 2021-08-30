3a85d2e080d1e1d988ffbb52d81836b2a14b5731
CassHsu <cass.hsu@gmail.com>
2021-08-30 18:35:23 +0800

    Create 1800-01.js

---
+++ 1800/1800-01.js
@@ -0,0 +1,16 @@
+var maxAscendingSum = function(nums) {
+    let count = nums[0];
+    let ans = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i-1]) {
+            count += nums[i];
+        } else {
+            count = nums[i];
+        }
+        
+        ans = Math.max(ans, count);
+    }
+    
+    return ans;
+};
