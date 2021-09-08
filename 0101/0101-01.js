bd21c6a395a601fbd86526bfdd5bb756a1d27fa6
CassHsu <cass.hsu@gmail.com>
2021-09-08 09:35:44 +0800

    Recursion

---
+++ 0101/0101-01.js
@@ -0,0 +1,18 @@
+var isSymmetric = function(root) {
+    if (!root) return false;
+    return same(root.left, invert(root.right));
+};
+
+const invert = (root) => {
+    if (!root) return;
+    [root.left, root.right] = [invert(root.right), invert(root.left)];
+    return root;
+}
+
+const same = (m, n) => {
+    if (!m && !n) return true;
+    if (!m || !n) return false;
+    if (m.val !== n.val) return false;
+    
+    return same(m.left, n.left) && same(m.right, n.right);
+}
