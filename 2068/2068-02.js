74ec3abd845c44153b851612869b2bda48f725c4
CassHsu <cass.hsu@gmail.com>
2021-11-20 21:06:17 +0800

    Map + Set

---
+++ 2068/2068-02.js
@@ -0,0 +1,30 @@
+var checkAlmostEquivalent = function(word1, word2) {
+    const c1 = new Map();
+    const c2 = new Map();
+    const s = new Set(word1 + word2);
+    
+    for (const w of word1) {
+        if (c1.has(w)) {
+            c1.set(w, c1.get(w) + 1);
+        } else {
+            c1.set(w, 1);
+        }
+    }
+    
+    for (const w of word2) {
+        if (c2.has(w)) {
+            c2.set(w, c2.get(w) + 1);
+        } else {
+            c2.set(w, 1);
+        }
+    }
+    
+    for (const w of s) {
+        const v1 = c1.has(w) ? c1.get(w) : 0;
+        const v2 = c2.has(w) ? c2.get(w) : 0;
+        
+        if (Math.abs(v1 - v2) > 3) return false;
+    }
+    
+    return true;
+};
