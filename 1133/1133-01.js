3459d82f19fe6a673fa0923fcaf9ab42fa0d0dcd
CassHsu <cass.hsu@gmail.com>
2021-12-12 10:34:57 +0800

    Create 1133-01.js

---
+++ 1133/1133-01.js
@@ -0,0 +1,15 @@
+var largestUniqueNumber = function(nums) {
+    const m = new Map();
+    const singles = [];
+    
+    nums.forEach(n => {
+        if (m.has(n)) m.set(n, m.get(n) + 1);
+        else m.set(n, 1);
+    });
+    
+    m.forEach((v, k) => {
+        if (v === 1) singles.push(k);
+    });
+    
+    return singles.length > 0 ? Math.max(...singles) : -1;
+};
