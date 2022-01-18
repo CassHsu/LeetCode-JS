0b53923f736b0d81f02c315dd01247a10bc30754
CassHsu <cass.hsu@gmail.com>
2022-01-18 21:24:04 +0800

    Create 2129-01.js

---
+++ 2129/2129-01.js
@@ -0,0 +1,14 @@
+var capitalizeTitle = function(title) {
+    const ans = [];
+    const words = title.split(" ");
+                              
+    for (const word of words) {
+        let w = word.toLowerCase();
+        if (w.length > 2) {
+            w = w[0].toUpperCase() + w.slice(1);
+        }
+        ans.push(w);
+    }
+    
+    return ans.join(" ");
+};
