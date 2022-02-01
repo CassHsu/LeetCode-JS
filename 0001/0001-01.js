ee3ae950f50580ad72d561b43c9d6f104df5b0c5
CassHsu <cass.hsu@gmail.com>
2022-02-01 09:23:06 +0800

    Create 0001-01.js

---
+++ 0001/0001-01.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    const m = new Map();
+    
+    for (const [i, n] of nums.entries()) {
+        const find = target - n;
+        if (m.has(find)) return [m.get(find), i];
+        
+        m.set(n, i);
+    }
+    
+    return [-1, -1];
+};
