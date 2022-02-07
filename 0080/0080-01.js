b7efed59da0b8726facda98b7e332473158a161e
CassHsu <cass.hsu@gmail.com>
2022-02-07 21:16:58 +0800

    Create 0080-01.js

---
+++ 0080/0080-01.js
@@ -0,0 +1,19 @@
+var removeDuplicates = function(nums) {
+    let count = 1;
+    let curr = 1;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+        
+        if (count <= 2) {
+            nums[curr] = nums[i];
+            curr++;
+        }
+    }
+    
+    return curr;
+};
