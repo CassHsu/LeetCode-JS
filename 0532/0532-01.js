27bd8db50a683ea270e9b8538927f6e55096fc69
CassHsu <cass.hsu@gmail.com>
2022-02-10 23:08:48 +0800

    Map

---
+++ 0532/0532-01.js
@@ -0,0 +1,14 @@
+var findPairs = function(nums, k) {
+    let count = 0;
+    const m = new Map();
+    
+    nums.forEach(n => {
+        if (m.has(n)) m.set(n, m.get(n) + 1);
+        else m.set(n, 1);
+    });
+    
+    if (k > 0) m.forEach((_, key) => { if (m.has(key + k)) count++;});
+    if (k === 0) m.forEach((val, _) => { if (val > 1) count++;});
+    
+    return count;
+};
