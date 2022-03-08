c742a95522c7955ab9d37179ea9204ea2585bf88
CassHsu <cass.hsu@gmail.com>
2022-03-08 09:23:10 +0800

    Update 2190-01.js

--- 2190/2190-01.js
+++ 2190/2190-01.js
@@ -1,31 +1,22 @@
 var mostFrequent = function(nums, key) {
     const m = new Map();
-    const targets = [];
     
     for (let i = 1; i < nums.length; i++) {
         if (nums[i - 1] === key) {
-            targets.push(nums[i]);
+            if (m.has(nums[i])) m.set(nums[i], m.get(nums[i]) + 1);
+            else m.set(nums[i], 1);
         }
     }
     
-    if (targets.length === 1) return targets[0];
-    
-    for (const t of targets) {
-        if (m.has(t)) {
-            m.set(t, m.get(t) + 1);
-        } else {
-            m.set(t, 1);
-        }
-    }
-    
-    let max = 0;
     let ans = nums[0];
+    let max = 0;
+    
     m.forEach((v, k) => {
         if (v > max) {
             max = v;
             ans = k;
         }
-    });
+    })
     
     return ans;
 };
