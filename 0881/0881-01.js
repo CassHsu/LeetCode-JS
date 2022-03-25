18432c341f73f9834cbae7359e94a2101b6db844
CassHsu <cass.hsu@gmail.com>
2022-03-25 22:02:44 +0800

    Two Pointer

---
+++ 0881/0881-01.js
@@ -0,0 +1,16 @@
+var numRescueBoats = function(people, limit) {
+    let count = 0;
+    people.sort((a, b) => a - b);
+    let i = 0;
+    let j = people.length - 1;
+    
+    while (i <= j) {
+        count++;
+        if (people[i] + people[j] <= limit) {
+            i++;
+        }
+        j--;
+    }
+    
+    return count;
+};
