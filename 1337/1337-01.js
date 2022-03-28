e2b345db8b450243505afe08e08b6820dd0d4a46
CassHsu <cass.hsu@gmail.com>
2022-03-28 09:13:30 +0800

    Linear Search and Sorting

---
+++ 1337/1337-01.js
@@ -0,0 +1,15 @@
+var kWeakestRows = function(mat, k) {
+    const counts = [];
+    
+    for (const [i, row] of mat.entries()) {
+        counts.push([i, row.reduce((a, b) => a + b, 0)]);
+    }
+    
+    counts.sort((a, b) => a[1] - b[1]);
+    
+    const ans = [];
+    for (const c of counts.slice(0, k)) {
+        ans.push(c[0]);
+    }
+    return ans;
+};
