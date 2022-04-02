e5af15771f3a41e46f4063875e4fcfcb452f93e7
CassHsu <cass.hsu@gmail.com>
2022-04-02 10:43:46 +0800

    Two Pointers

---
+++ 0344/0344-01.js
@@ -0,0 +1,14 @@
+var reverseString = function(s) {
+    let l = 0;
+    let r = s.length - 1;
+    
+    while (l < r) {
+        let tmp = s[l];
+        s[l] = s[r];
+        s[r] = tmp;
+        
+        
+        l++;
+        r--;
+    }
+};
