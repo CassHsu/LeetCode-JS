1e33b290f44e290c13103e24380772ccb2868743
CassHsu <cass.hsu@gmail.com>
2022-04-05 17:42:34 +0800

    Two Pointers

---
+++ 0031/0031-01.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    const swap = (nums, i, j) => { 
+        const t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+    
+    const reverse = (nums, i) => {
+        let j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+    
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+    
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (nums[i] >= nums[j]) j--;
+        
+        swap(nums, i, j);
+    }
+    
+    reverse(nums, i + 1);
+};
