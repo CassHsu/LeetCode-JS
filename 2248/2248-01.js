231316aa260007b53bd7947e4c8a31416194747c
CassHsu <cass.hsu@gmail.com>
2022-04-28 09:46:11 +0800

    Set & Sorting

---
+++ 2248/2248-01.js
@@ -0,0 +1,10 @@
+var intersection = function(nums) {
+    let r = new Set(nums[0]);
+    
+    for (let i = 1; i < nums.length; i++) {
+        const ns = new Set(nums[i]);
+        r = new Set([...r].filter(x => ns.has(x)));
+    }
+    
+    return [...r].sort((a, b) => a - b);
+};
