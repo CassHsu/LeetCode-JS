0e002f3d546d38363c398779f1ddfb7508c2a1b3
CassHsu <cass.hsu@gmail.com>
2022-05-01 11:34:01 +0800

    Map

---
+++ 1165/1165-01.js
@@ -0,0 +1,16 @@
+var calculateTime = function(keyboard, word) {
+    const m = new Map();
+    
+    for (let i = 0; i < keyboard.length; i++) {
+        m.set(keyboard.charAt(i), i);
+    }
+    
+    let count = 0;
+    let curr = 0;
+    for (const w of word) {
+        count += Math.abs(m.get(w) - curr);
+        curr = m.get(w);
+    }
+    
+    return count;
+};
