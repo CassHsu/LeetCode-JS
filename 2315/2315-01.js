d235ace0bc4ca52a6180cec41a6549f7b554b8e9
CassHsu <cass.hsu@gmail.com>
2022-10-12 21:42:35 +0800

    One-Pass

---
+++ 2315/2315-01.js
@@ -0,0 +1,19 @@
+var countAsterisks = function(s) {
+    let count = 0;
+    let bars = 0;
+    
+    for (const c of s) {
+        switch(c) {
+            case '*':
+                if (bars % 2 == 0) count++;
+                break;
+            case '|':
+                bars++;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return count;
+};
