0c4462fe95630f1bf0d0b8de29f4b42ec3d80cc0
CassHsu <cass.hsu@gmail.com>
2022-10-14 13:46:06 +0800

    Sorting

---
+++ 0976/0976-01.js
@@ -0,0 +1,11 @@
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    for (let i = nums.length - 3; i > -1; i--) {
+        if (nums[i] + nums[i + 1] > nums[i + 2]) {
+            return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+    }
+    
+    return 0;
+};
