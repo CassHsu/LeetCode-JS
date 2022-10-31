7d1a31d11a903d65eac583516713d3950c36bfb9
CassHsu <cass.hsu@gmail.com>
2022-10-31 10:14:28 +0800

    Two Pass

---
+++ 2441/2441-01.js
@@ -0,0 +1,18 @@
+var findMaxK = function(nums) {
+    const m = new Map();
+    const pn = [];
+    
+    nums.forEach(n => {
+        if (m.has(n)) m.set(n, m.get(n) + 1);
+        else m.set(n, 1);
+    });
+    
+    let max = -1;
+    nums.forEach(n => {
+        if (n > 0 && m.has(-n)) {
+            if (n > max) max = n;
+        }
+    });
+    
+    return max
+};
