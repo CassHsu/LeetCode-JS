22cbcde8b3e501cdb6467869698908f7d29deded
CassHsu <cass.hsu@gmail.com>
2022-11-04 12:17:10 +0800

    One Pass

--- 2441/2441-01.js
+++ 2441/2441-01.js
@@ -1,17 +1,15 @@
 var findMaxK = function(nums) {
     const m = new Map();
+    let res = -1;
     
     nums.forEach(n => {
         if (m.has(n)) m.set(n, m.get(n) + 1);
         else m.set(n, 1);
-    });
-    
-    let max = -1;
-    nums.forEach(n => {
-        if (n > 0 && m.has(-n)) {
-            if (n > max) max = n;
+        
+        if (m.has(-n)) {
+            res = Math.max(res, Math.abs(n));
         }
     });
     
-    return max
+    return res
 };
