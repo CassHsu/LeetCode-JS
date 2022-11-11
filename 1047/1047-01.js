5a1f4daa44b3bf4e20ab46dade3a872b50e8ac77
CassHsu <cass.hsu@gmail.com>
2022-11-11 18:07:10 +0800

    Stack

---
+++ 1047/1047-01.js
@@ -0,0 +1,15 @@
+var removeDuplicates = function(s) {
+    const res = [];
+    
+    for (const c of s) {
+        const size = res.length;
+
+        if (size > 0 && res[size - 1] === c) {
+            res.pop();
+        } else {
+            res.push(c);
+        }
+    }
+    
+    return res.join("");
+};
