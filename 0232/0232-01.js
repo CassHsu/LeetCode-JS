8b88c47f178e81de66dabbbdb84353c13b9473d6
CassHsu <cass.hsu@gmail.com>
2022-12-18 22:49:00 +0800

    Update 0232-01.js

--- 0232/0232-01.js
+++ 0232/0232-01.js
@@ -1,24 +1,21 @@
-class MyQueue {
-    stack
-    constructor() {
-        this.stack = []
-    }
+var MyQueue = function() {
+    this.stack = [];
+};
 
-    push(x: number): void {
-        this.stack.push(x)
-    }
+MyQueue.prototype.push = function(x) {
+    this.stack.push(x);
+};
 
-    pop(): number {
-        const x = this.stack[0]
-        this.stack = this.stack.slice(1, this.stack.length)
-        return x
-    }
+MyQueue.prototype.pop = function() {
+    const x = this.stack[0];
+    this.stack = this.stack.slice(1, this.stack.length);
+    return x;
+};
 
-    peek(): number {
-        return this.stack[0]
-    }
+MyQueue.prototype.peek = function() {
+    return this.stack[0];
+};
 
-    empty(): boolean {
-        return this.stack.length === 0 ? true: false
-    }
-}
+MyQueue.prototype.empty = function() {
+    return this.stack.length == 0 ? true: false;
+};
