266c8a9441d5766da069dd2f62dd68ee8110f0d4
CassHsu <cass.hsu@gmail.com>
2022-12-20 09:37:33 +0800

    Create 2485-01.js

---
+++ 2485/2485-01.js
@@ -0,0 +1,16 @@
+var pivotInteger = function(n) {
+    const nums = [];
+
+    for (let i = 0; i <= n; i++) {
+        nums.push(i);
+    }
+
+    for (let p = 1; p <= n + 1; p++) {
+        const ls = nums.slice(1, p + 1).reduce((x, y) => x + y, 0);
+        const rs = nums.slice(p, n + 1).reduce((x, y) => x + y, 0);
+
+        if (ls === rs) return p;
+    }
+
+    return -1;
+};
