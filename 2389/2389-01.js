20cf2c9c312a2df1db5eeb63855f5a215456c30b
CassHsu <cass.hsu@gmail.com>
2022-12-27 09:45:01 +0800

    Create 2389-01.js

---
+++ 2389/2389-01.js
@@ -0,0 +1,21 @@
+var answerQueries = function(nums, queries) {
+    nums.sort((a, b) => a - b);
+    const ans = [];
+
+    for (let q of queries) {
+        let count = 0;
+
+        for (let n of nums) {
+            if (q >= n) {
+                q -= n;
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        ans.push(count);
+    }
+
+    return ans;
+};
