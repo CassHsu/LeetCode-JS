5bdd2983c5b1ebf4fd600ce389b8ec0568afb4c4
CassHsu <cass.hsu@gmail.com>
2023-01-21 20:40:47 +0800

    Create 2535-01.js

---
+++ 2535/2535-01.js
@@ -0,0 +1,16 @@
+var differenceOfSum = function(nums) {
+    let es = 0;
+    let ds = 0;
+    let digits = "";
+
+    nums.forEach(n => {
+        es += n;
+        digits += n.toString();
+    });
+
+    for (const d of digits) {
+        ds += parseInt(d);
+    }
+
+    return Math.abs(es - ds);
+};
