56e99e56f5ae5083f92d87b64a2841564d1a2080
CassHsu <cass.hsu@gmail.com>
2023-02-09 23:11:53 +0800

    Create 1470-01.js

---
+++ 1470/1470-01.js
@@ -0,0 +1,12 @@
+var shuffle = function(nums, n) {
+    const n1 = nums.slice(0, n);
+    const n2 = nums.slice(n);
+    const ans = [];
+    
+    for (let i = 0; i < n1.length; i++) {
+        ans.push(n1[i]);
+        ans.push(n2[i]);
+    }
+    
+    return ans;
+};
