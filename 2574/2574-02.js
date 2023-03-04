fd7f461595f14b68e396230178b81168328d87ed
CassHsu <cass.hsu@gmail.com>
2023-03-04 23:41:21 +0800

    Create 2574-02.js

---
+++ 2574/2574-02.js
@@ -0,0 +1,15 @@
+var leftRigthDifference = function(nums) {
+    const ans = new Array(nums.length).fill(0);
+
+    let ls = 0;
+    let rs = 0;
+    nums.forEach((n) => rs += n);
+
+    for(let i = 0; i < nums.length; i++) {
+        rs -= nums[i]; 
+        ans[i] = Math.abs(ls - rs); 
+        ls += nums[i];                   
+    }
+
+    return ans;
+};
