3d1c3496d04284fd9b511a4217a22a646c064411
CassHsu <cass.hsu@gmail.com>
2023-03-18 15:55:39 +0800

    Create 2586-01.js

---
+++ 2586/2586-01.js
@@ -0,0 +1,14 @@
+var vowelStrings = function(words, left, right) {
+    let count = 0;
+    const vowels = {
+        'a': true, 'e': true, 'i': true, 'o': true, 'u': true
+    }
+
+    for (let i = left; i <= right; i++) {
+        const w = words[i];
+        const end = w.length - 1;
+        if (vowels[w[0]] && vowels[w[end]]) count++;
+    }
+
+    return count;
+};
