244bf6dd8704e9fba860fea434cf456db5fa822a
CassHsu <cass.hsu@gmail.com>
2023-07-04 22:41:51 +0800

    Create 2744-02.js

---
+++ 2744/2744-02.js
@@ -0,0 +1,14 @@
+var maximumNumberOfStringPairs = function(words) {
+    let count = 0;
+    const n = words.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (words[i][0] == words[j][1] && words[i][1] == words[j][0]) {
+                count++;
+            }
+        }
+    }    
+
+    return count;
+};
