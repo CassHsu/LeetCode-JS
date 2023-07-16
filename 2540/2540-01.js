40c0b39c13df32eae6962564f07d59178babedfa
CassHsu <cass.hsu@gmail.com>
2023-07-16 21:19:52 +0800

    Create 2540-01.js

---
+++ 2540/2540-01.js
@@ -0,0 +1,17 @@
+var getCommon = function(nums1, nums2) {
+    let i = 0;
+    let j = 0;
+    const n = nums1.length;
+    const m = nums2.length;
+
+    while (i < n && j < m) {
+        if (nums1[i] < nums2[j]) {
+            i++;
+        } else if (nums1[i] > nums2[j]) {
+            j++;
+        } else {
+            return nums1[i];
+        }
+    }
+
+    return -1;
