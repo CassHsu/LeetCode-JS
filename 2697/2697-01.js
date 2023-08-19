dec60e1c4780b74469e74adcc9580be999e179f2
CassHsu <cass.hsu@gmail.com>
2023-08-19 22:18:45 +0800

    Create 2697-01.js

---
+++ 2697/2697-01.js
@@ -0,0 +1,30 @@
+var makeSmallestPalindrome = function(s) {
+    const m = new Map();
+    const res = [];
+    let l = 0;
+    let r = s.length - 1;
+
+    while (l < r) {
+        if (s[l] != s[r]) {
+            if (s[l] > s[r]) {
+                m.set(l, s[r]);
+            } else {
+                m.set(r, s[l]);
+            }
+        }
+
+        l++;
+        r--;
+    }
+
+    console.log(m);
+    for (let i = 0; i < s.length; i++) {
+        const v = m.get(i)
+        if (v) {
+            res.push(v);
+        } else {
+            res.push(s[i]);
+        }
+    }
+    return res.join("");
+};
