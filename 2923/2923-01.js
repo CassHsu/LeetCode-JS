53ad801232d8e4aa4541f448923acfb0c598ff2b
CassHsu <cass.hsu@gmail.com>
2023-11-10 23:26:13 +0800

    Create 2923-01.js

---
+++ 2923/2923-01.js
@@ -0,0 +1,15 @@
+var findChampion = function(grid) {
+    const n = grid.length;
+    const m = grid[0].length;
+
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        for (j = 0; j < m; j++) {
+            sum += grid[i][j];
+        }
+
+        if (sum == n - 1) return i;
+    }
+    
+    return 0;    
+};
