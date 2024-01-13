d14941023d0bb21407ccbb90fd3aad3f4dd7dda9
CassHsu <cass.hsu@gmail.com>
2024-01-13 23:34:35 +0800

    Create 0455-02.js

---
+++ 0455/0455-02.js
@@ -0,0 +1,20 @@
+var findContentChildren = function(g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    
+    let count = 0;
+    let gi = g.length - 1;
+    let si = s.length - 1;
+
+    while (gi >= 0 && si >= 0) {
+        if (s[si] >= g[gi]) {
+            count++;
+            gi--;
+            si--;
+        } else {
+            gi--;
+        }
+    }
+    
+    return count;
+};
