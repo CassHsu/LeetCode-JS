c2c0bd5d6bdd198d569cdf0a00f77b3e5162e462
CassHsu <cass.hsu@gmail.com>
2024-01-24 23:02:47 +0800

    Create 3005-02.js

---
+++ 3005/3005-02.js
@@ -0,0 +1,19 @@
+var maxFrequencyElements = function(nums) {
+    const m = new Map();
+    let max = 0;
+    let count = 0;
+
+    for (const n of nums) {
+        const f = m.get(n) + 1 || 1;
+        console.log(f);
+        if (f === max) count++;
+        else if (f > max) {
+            max = f;
+            count = 1;
+        }
+
+        m.set(n, f);
+    }
+
+    return max * count;
+};
